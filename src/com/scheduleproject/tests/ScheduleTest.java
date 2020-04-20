5a52c6347d7975acdee5896de83415f5ab5e14cf
Noemy Roberta <noemy.oliveira2@outlook.com>
2020-04-19 23:55:27 -0300

    test added

---
+++ src/com/scheduleproject/tests/ScheduleTest.java
@@ -0,0 +1,14 @@
+package com.scheduleproject.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleTest {
+
+    @org.junit.jupiter.api.Test
+    void addContact() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getByName() {
+    }
+}
